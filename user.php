2c0a26e06e4832935c4746eac74d14a99f52087e
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2023-06-13 19:56:54 +0300

    add order functionality

--- user.php
+++ user.php
@@ -97,7 +97,6 @@ $products->execute();
 
 
 
-
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 	<script>
@@ -126,6 +125,7 @@ $products->execute();
 			})
 		})
 	</script>
+
 
 </body>
 

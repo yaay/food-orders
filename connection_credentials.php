4d3c9444007e8ee4e25849da30bc060c21ca37b5
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2023-06-13 19:55:19 +0300

    add user's order page

--- connection_credentials.php
+++ connection_credentials.php
@@ -4,5 +4,4 @@ $db_user = 'root';
 $db_password = '';
 $db_name = 'restaurant';
 $db_host = 'localhost';
-$db_port = 3306;
-?>
+$db_port = '3306';
--- user.php
+++ user.php
@@ -1,88 +1,132 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet"  href="css_bootstrap/bootstrap.min.css">
-    <link rel="stylesheet"  href="css_bootstrap/bootstrap.css">
-</head>
-<body>
 <?php
 session_start();
 
+include './db_connection.php';
+include './connection_credentials.php';
+
 $username = $_SESSION["username"];
+
+$db = connect_to_database($db_user, $db_password, $db_name, $db_host, $db_port);
+
+$products = $db->prepare("SELECT * FROM `restaurant`.`products`");
+$products->execute();
+
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-      <a href="logout.php" class="btn btn-danger" style=" text-decoration: none; font-weight: bold;  text-align: center; border: 3px solid black;">Logout</a>
-      &nbsp;&nbsp;
-        <img src="./images/social.jpg" style="width: 50px; height:50px; border-radius:5px;">&nbsp;&nbsp;&nbsp;
-        <h6 style="margin-top:15px;"><?php echo $username ?></h6>
-      </form>
-    </div>
-  </div>
-</nav>
 
-<br>
-<br>
-<div class="container">
-  <div class="row">
-    <div class="col-md-9">
-    <div class="card-group">
-  <div class="card mx-1 rounded">
-    <img src="./images/social.jpg" class="card-img-top rounded" alt="..." height="100%">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-    </div>
-  </div>
-  <div class="card mx-1 border rounded">
-    <img src="./images/social.jpg" class="card-img-top rounded" alt="..." height="100%">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-    </div>
-  </div>
-  <div class="card mx-1 border rounded">
-    <img src="./images/social.jpg" class="card-img-top rounded" alt="..." height="100%">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-    </div>
-  </div>
-  <div class="card mx-1 border rounded">
-    <img src="./images/social.jpg" class="card-img-top rounded" alt="..." height="100%">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-    </div>
-  </div>
-</div>
-    </div>
-    <div class="col-6 col-md-3">
-        <div style="border: 1px solid black; padding:10px; border-radius:7px;">
-            <p style="text-align:center;">Tea&nbsp;&nbsp;<span class="btn btn-success">+</span>&nbsp;&nbsp;<span>EGP   </span>&nbsp;&nbsp;<span class="btn btn-warning">-</span></p>
-            <p>Tea</p>
-        </div>
-    </div>
-  </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="css_bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="css_bootstrap/bootstrap.css">
+</head>
+
+<body>
 
 
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Products</a>
+					</li>
+				</ul>
+				<form class="d-flex">
+					<a href="logout.php" class="btn btn-danger" style=" text-decoration: none; font-weight: bold;  text-align: center; border: 3px solid black;">Logout</a>
+					&nbsp;&nbsp;
+					<img src="./images/social.jpg" style="width: 50px; height:50px; border-radius:5px;">&nbsp;&nbsp;&nbsp;
+					<h6 style="margin-top:15px;"><?php echo $username ?></h6>
+				</form>
+			</div>
+		</div>
+	</nav>
+
+	<br>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="card-group">
+					<div class="row">
+						<?php while ($product = $products->fetch(PDO::FETCH_ASSOC)) : ?>
+							<div class="card mx-1 col-3 rounded">
+								<div class="card-title"><?= $product["name"] ?></div>
+								<div class="card-body">
+									<img src="uploaded_img/<?= $product["image"] ?>" width="100" alt="">
+									<div class="mt-2">
+										<button style="z-index:1212" data-id="<?= $product["ID"] ?>" data-name="<?= $product["name"] ?>" class="btn btn-primary btn-sm add-btn">add</button>
+									</div>
+								</div>
+
+							</div>
+						<?php endwhile ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-3">
+				<div style="border: 1px solid black; padding:10px; border-radius:7px;">
+					<div id="orderd-products-injection-point">
+						<div class="row">
+							<div class="col-6">
+								<p class="mt-2">Tea <soan class="mx-2 item-count text-danger">2</span></p>
+
+							</div>
+							<div class="col-6">
+								<button class="btn btn-primary">+</button>
+								<button class="btn btn-warning">-</button>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+	<script>
+		const addBtns = document.querySelectorAll(".add-btn");
+		const orderProductHtml = (productId, productName) => {
+			return `
+						<div class="row" id="${productName}-${productId}">
+							<div class="col-6">
+								<p class="mt-2">${productName} <soan class="mx-2 item-count text-danger">1</span></p>
+
+							</div>
+							<div class="col-6">
+								<button class="btn increament btn-primary">+</button>
+								<button class="btn decreament btn-warning">-</button>
+							</div>
+						</div>
+			`;
+		}
+		addBtns.forEach(btn => {
+			btn.addEventListener("click", event => {
+
+				const orderProduct = document.getElementById("orderd-products-injection-point");
+
+				orderProduct.insertAdjacentHTML("beforeend", orderProductHtml(event.target.dataset.id, event.target.dataset.name));
+
+			})
+		})
+	</script>
+
 </body>
+
 </html>
